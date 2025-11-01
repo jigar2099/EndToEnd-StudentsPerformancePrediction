67c2b7559526ace46280c02502431d98d51014d2
jigar2099 <jigarbhanderi@gmail.com>
2025-11-01 21:57:52 +0100

    DataTransformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 # Anchor artifacts directory to the src folder (one level up from components)
 SRC_DIR = os.path.dirname(os.path.dirname(__file__))
 ARTIFACTS_DIR = os.path.join(SRC_DIR, "artifacts")
@@ -46,4 +49,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
